2164d9a954a608e3c17b36f5078a841606e346ed
viniciusdemoraess <demoraesvini@outlook.com>
2021-05-30 20:10:22 -0400

    Criando o db

--- src/db/init.js
+++ src/db/init.js
@@ -1,7 +1,72 @@
-const Database = require('config')
+const Database = require('./config')
 
-Database()
+const initDb = {
+    async init(){
+  
+        const db = await Database()
 
-Database.exec();
+        await db.exec(`CREATE TABLE profile (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            avatar TEXT,
+            monthly_budget INT,
+            days_per_week INT,
+            hours_per_day INT,
+            vacation_per_year INT,
+            value_hour INT
+        )`);
 
-Database.close()
+        await db.exec(`CREATE TABLE jobs (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            daily_hours TEXT,
+            total_hours INT,
+            created_at DATETIME
+
+        )`);
+
+        await db.run(`INSERT INTO profile (
+            name,
+            avatar,
+            monthly_budget,
+            days_per_week,
+            hours_per_day,
+            vacation_per_year
+        ) VALUES (
+            "Vinícius de Moraes",
+            "https://github.com/viniciusdemoraess.png",
+            3000,
+            5,
+            5,
+            4
+        );`)
+
+        await db.run(`INSERT INTO jobs(
+            name,
+            daily_hours,
+            total_hours,
+            created_at
+        ) VALUES(
+            "Pizzaria Guloso",
+            2,
+            1,
+            1617514376018
+        );`)
+
+        await db.run(`INSERT INTO jobs(
+            name,
+            daily_hours,
+            total_hours,
+            created_at
+        ) VALUES(
+            "One Two Projects",
+            3,
+            47,
+            1617514376018
+        );`)
+
+        await db.close()  
+    }
+}
+
+initDb.init()
